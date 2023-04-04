48b643f3c8844a4d02817203f2e7a6892e829797
MSRbuddy <meghanadasari222@gmail.com>
2023-04-04 16:59:37 +0530

    Added: If-Else statements!

---
+++ CastingVote/CastingVote/CastingVote.cs
@@ -0,0 +1,18 @@
+﻿namespace CastingVote
+{
+    internal class CastingVote
+    {
+        static void Main(string[] args)
+        {
+            int vote_age;
+            Console.WriteLine("Enter the age of the candidate : ");
+            vote_age = Convert.ToInt32(Console.ReadLine());
+            if (vote_age >= 18)
+            {
+                Console.WriteLine("You are eligible for casting your vote");
+            }
+            else
+                Console.WriteLine("You are not eligible to caste your vote");           
+        }
+    }
+}
---
+++ EqualOrNot/EqualOrNot/EqualOrNot.cs
@@ -0,0 +1,25 @@
+﻿namespace EqualOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1, int2;
+            Console.WriteLine("Check whether two integers are equal or not:");
+            Console.WriteLine("Enter 1st number: ");
+            int1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd number: ");
+            int2 = Convert.ToInt32(Console.ReadLine());
+
+            if (int1 == int2)
+            {
+                Console.WriteLine("{0} and {1} are equal", int1, int2);
+            }              
+            else
+            {
+                Console.WriteLine("{0} and {1} are not equal", int1, int2);
+            }    
+        }
+    }
+}
