91ddf3a97221a1d28d616af908d19a6d04d7a90e
MSRbuddy <meghanadasari222@gmail.com>
2023-04-04 17:06:27 +0530

    Added: SwitchCase statements!

---
+++ ArithmeticOperations/ArithmeticOperations/ArithmeticOperations.cs
@@ -0,0 +1,41 @@
+﻿namespace ArithmeticOperations
+{
+    internal class ArithmeticOperations
+    {
+        static void Main(string[] args)
+        {
+            //Reading First Number  
+            Console.Write("Enter No1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            //Reading Second Number  
+            Console.Write("Enter No2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("1.Addition");
+            Console.WriteLine("2.Subtraction");
+            Console.WriteLine("3.Divsion");
+            Console.WriteLine("4.Multiplication");
+            
+            //Reading a Choice  
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Addition Of Two Numbers is : " + (a + b));
+                    break;
+                case 2:
+                    Console.WriteLine("Subtraction Of Two Numbers is : " + (a - b));
+                    break;
+                case 3:
+                    Console.WriteLine("Division Of Two Numbers is : " + (a / b));
+                    break;
+                case 4:
+                    Console.WriteLine("Multiplicaion Of Two Numbers is : " + (a * b));
+                    break;
+                default:
+                    Console.WriteLine("Invali Entry: Choose 1-4");
+                    break;
+            }
+        }
+    }
+}
---
+++ WeekDay/WeekDay/WeekDay.cs
@@ -0,0 +1,43 @@
+﻿namespace WeekDay
+{
+    internal class WeekDay
+    {
+        static void Main(string[] args)
+        {
+            int weekday;
+  
+            // Enter weekday number
+            Console.WriteLine("Enter weekday number (1-7): ");
+            weekday = Convert.ToInt32(Console.ReadLine());
+  
+            // Using switch case to validate
+            switch (weekday)
+            {
+                case 1:
+                    Console.WriteLine("It is SUNDAY");
+                    break;
+                case 2:
+                    Console.WriteLine("It is MONDAY");
+                    break;
+                case 3:
+                    Console.WriteLine("It is TUESDAY");
+                    break;
+                case 4:
+                    Console.WriteLine("It is WEDNESDAY");
+                    break;
+                case 5:
+                    Console.WriteLine("It is THURSDAY");
+                    break;
+                case 6:
+                    Console.WriteLine("It is FRIDAY");
+                    break;
+                case 7:
+                    Console.WriteLine("It is SATURDAY");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+            }
+        }
+    }
+}
